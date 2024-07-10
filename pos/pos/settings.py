5e6f2c5dd2cd85ffb4972623244b7a520a419911
chijin GUI <chijin.gui@insa-lyon.fr>
2024-07-10 09:49:15 +0200

    update port mysql

--- pos/pos/settings.py
+++ pos/pos/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'USER':'root',
         'PASSWORD':'',
         'HOST':'localhost',
-        'PORT':'3316',
+        'PORT':'3306',
         'OPTIONS':{
             # 'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
             'sql_mode':'traditional',
